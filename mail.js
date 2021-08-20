4603cb5698519c3265ee87e2edb6b2b33a1209c6
priyabrata-cpp <76444246+priyabrata-cpp@users.noreply.github.com>
2021-08-20 22:09:06 +0530

    Update mail.js

--- mail.js
+++ mail.js
@@ -10,7 +10,7 @@ window.addEventListener("DOMContentLoaded", function () {
     function success() {
       form.reset();
       status.classList.add("success");
-      status.innerHTML = "Thanks!";
+
       window.location = "https://www.manobik.cf/mail1/mail.html";
     }
   
