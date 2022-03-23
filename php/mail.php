de75d5b9fb4f8036564745d1600e645574e47260
priyabrata-cpp <76444246+priyabrata-cpp@users.noreply.github.com>
2022-03-23 13:12:18 +0530

    Update mail.php

--- php/mail.php
+++ php/mail.php
@@ -3,7 +3,7 @@
 /* =====================================================
  * change this to the email you want the form to send to
  * ===================================================== */
-$email_to = "you@company.pw"; 
+$email_to = "manobik229@gmail.com"; 
 $email_from = "webmaster@company.pw"; // must be different than $email_from 
 $email_subject = "Contact Form submitted";
 
